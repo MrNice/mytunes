20bc962c63d2cb5b7eadeb29d49d7728bf08f093
Nicholas Van de walle <nicholas@niceguys.co>
2014-03-12 16:28:48 -0700

    Scaffolded Queue Views, fixed typos

--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -1,8 +1,8 @@
 // SongModel.js - Defines a backbone model class for songs.
 var SongModel = Backbone.Model.extend({
   defaults: {
-    thumbs = 'false',
-    image = 'http://upload.wikimedia.org/wikipedia/en/3/38/GunsnRosesUseYourIllusionII.jpg',
+    thumbs : 'false',
+    image : 'http://upload.wikimedia.org/wikipedia/en/3/38/GunsnRosesUseYourIllusionII.jpg',
   },
 
   play: function(){
--- client/views/SongQueueEntryView.js
+++ client/views/SongQueueEntryView.js
@@ -1,4 +1,16 @@
 // SongQueueEntryView.js - Defines a backbone view class for the song queue entries.
 var SongQueueEntryView = Backbone.View.extend({
   // your code here!
+  tagName: 'span',
+  template: _.template('<span><%= artist + title %>'),
+
+  events: {
+    'click': function() {
+
+    }
+  },
+
+  render: function() {
+    return this.$el.html(this.template(this.model.attributes));
+  },
 });
