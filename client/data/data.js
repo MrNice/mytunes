03b01326b4ef5345e6386742587fb27e0c4e94ab
Nicholas Van de walle <nicholas@niceguys.co>
2014-03-12 14:32:09 -0700

    Added Weird Al Song to data

--- client/data/data.js
+++ client/data/data.js
@@ -19,5 +19,10 @@ var songData = [
     url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/06+If+Your+Girl+Only+Knew.mp3",
     title: "If Your Girl Only Knew",
     artist: "Aaliyah",
-  }
+  },
+  {
+    url: "http://media3.albinoblacksheep.com/albino_audio/Dont_Download_This_Song.mp3",
+    title: "Don't Download This Song",
+    artist: "Weird Al Yankovic"
+  },
 ];
