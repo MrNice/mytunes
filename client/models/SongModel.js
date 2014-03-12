24d911e700b870fa8e3aee03dfb07f4f3c78cf85
Nicholas Van de walle <nicholas@niceguys.co>
2014-03-12 14:50:21 -0700

    Added default thumbs and image parameters to SongModel

--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -1,5 +1,9 @@
 // SongModel.js - Defines a backbone model class for songs.
 var SongModel = Backbone.Model.extend({
+  defaults: {
+    thumbs = 'false',
+    image = 'http://upload.wikimedia.org/wikipedia/en/3/38/GunsnRosesUseYourIllusionII.jpg',
+  },
 
   play: function(){
     // Triggering an event here will also trigger the event on the collection
